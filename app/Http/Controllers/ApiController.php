718babe3f44b4469dad93c33ade286f623d2a42d
SiddhantaDutta1 <siddhantadutta1@gmail.com>
2022-10-19 15:06:23 +0530

    Register Api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use DB;
+class ApiController extends Controller
+{
+    public function register(Request $request)
+    {
+        $count=DB::table('users')->where('phone_no',$request->phone_no)->count();
+        if($count==0){
+            $table=new User;
+            $table->name=$request->name;
+            $table->phone_no=$request->phone_no;
+            $table->password=md5($request->password);
+            $table->save();
+            $data=[];
+            $message="You have successfully registered.";
+            $respo=200;
+        }else{
+            $data=[];
+            $message="This phone no already registered.";
+            $respo=200;
+        }
+
+         return response()->json([
+                'data' =>$data,
+              'message' => $message,
+            ], $respo);
+
+    }
+}
